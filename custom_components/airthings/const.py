01925bdc75e204f1497357be4c7c4c36e922f286
Fredrik Oterholt <fredrik@technologic.as>
2021-03-11 12:16:43 +0100

    Add more device types to examples. Cleanup sensor types.

--- custom_components/airthings/const.py
+++ custom_components/airthings/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import PERCENTAGE, TEMP_CELSIUS, \
     DEVICE_CLASS_TEMPERATURE, DEVICE_CLASS_PRESSURE, DEVICE_CLASS_ILLUMINANCE, \
     DEVICE_CLASS_TIMESTAMP, VOLUME_CUBIC_METERS
 
-DOMAIN = 'airthings'
+DOMAIN = "airthings"
 MANUFACTURER = "Airthings"
 OAUTH2_AUTHORIZE = "https://accounts.airthings.com/authorize"
 OAUTH2_TOKEN = "https://accounts-api.airthings.com/v1/token"
@@ -25,25 +25,70 @@ class AirthingsSensorType:
 
 
 SENSOR_TYPES = dict(
-    humidity=AirthingsSensorType('Humidity', "mdi:water-percent", 'humidity',
-                                 PERCENTAGE, DEVICE_CLASS_HUMIDITY),
-    temp=AirthingsSensorType('Temperature', "mdi:thermometer", 'temp', TEMP_CELSIUS,
-                             DEVICE_CLASS_TEMPERATURE),
-    co2=AirthingsSensorType('CO2', "mdi:molecule-co2", 'co2',
-                            CONCENTRATION_PARTS_PER_MILLION),
-    voc=AirthingsSensorType('TVOC', "mdi:chemical-weapon", 'voc',
-                            CONCENTRATION_PARTS_PER_BILLION),
-    pressure=AirthingsSensorType('Pressure', "mdi:gauge", 'pressure', PRESSURE_MBAR,
-                                 DEVICE_CLASS_PRESSURE),
-    light=AirthingsSensorType('Light level', "mdi:white-balance-sunny", 'light',
-                              PERCENTAGE, DEVICE_CLASS_ILLUMINANCE),
-    radonShortTermAvg=AirthingsSensorType('Radon Short term average', "mdi:atom",
-                                          'radonShortTermAvg',
-                                          f"Bq/{VOLUME_CUBIC_METERS} 24h avg"),
-    mold=AirthingsSensorType('Mold risk indicator', 'mdi:mushroom-outline', 'mold', '/ 10'),
-    virusRisk=AirthingsSensorType('Virus risk indicator', 'mdi:virus-outline', 'virusRisk', '/ 10'),
-    time=AirthingsSensorType('Last synced', "mdi:clock", 'time',
-                             device_class=DEVICE_CLASS_TIMESTAMP),
+    humidity=AirthingsSensorType(
+        name="Humidity",
+        icon="mdi:water-percent",
+        field="humidity",
+        unit=PERCENTAGE,
+        device_class=DEVICE_CLASS_HUMIDITY,
+    ),
+    temp=AirthingsSensorType(
+        name="Temperature",
+        icon="mdi:thermometer",
+        field="temp",
+        unit=TEMP_CELSIUS,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+    ),
+    co2=AirthingsSensorType(
+        name="CO2",
+        icon="mdi:molecule-co2",
+        field="co2",
+        unit=CONCENTRATION_PARTS_PER_MILLION,
+    ),
+    voc=AirthingsSensorType(
+        name="VOC",
+        icon="mdi:chemical-weapon",
+        field="voc",
+        unit=CONCENTRATION_PARTS_PER_BILLION,
+    ),
+    pressure=AirthingsSensorType(
+        name="Pressure",
+        icon="mdi:gauge",
+        field="pressure",
+        unit=PRESSURE_MBAR,
+        device_class=DEVICE_CLASS_PRESSURE,
+    ),
+    light=AirthingsSensorType(
+        name="Light level",
+        icon="mdi:white-balance-sunny",
+        field="light",
+        unit=PERCENTAGE,
+        device_class=DEVICE_CLASS_ILLUMINANCE,
+    ),
+    radonShortTermAvg=AirthingsSensorType(
+        name="Radon Short term average",
+        icon="mdi:atom",
+        field="radonShortTermAvg",
+        unit=f"Bq/{VOLUME_CUBIC_METERS} 24h avg",
+    ),
+    mold=AirthingsSensorType(
+        name="Mold risk indicator",
+        icon="mdi:mushroom-outline",
+        field="mold",
+        unit="/ 10",
+    ),
+    virusRisk=AirthingsSensorType(
+        name="Virus risk indicator",
+        icon="mdi:virus-outline",
+        field="virusRisk",
+        unit="/ 10",
+    ),
+    time=AirthingsSensorType(
+        name="Last synced",
+        icon="mdi:clock",
+        field="time",
+        device_class=DEVICE_CLASS_TIMESTAMP,
+    ),
 )
 
 
